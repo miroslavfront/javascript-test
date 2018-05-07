771fcb85c91a1f57778fcd9ee8e247b815b72828
Vislavski <wallsorim@yahoo.ca>
2018-05-07 19:16:31 +0200

    improve bugfix

--- src/bug_fix.js
+++ src/bug_fix.js
@@ -4,10 +4,8 @@ function fix_spelling(name) {
   // if (name = 'saturizd') // with single equality sign we asign the value...
   if (name === 'saturizd') {
     name = 'saturized';
-  } else {
-    return name;
-    // return fix_spelling(name); //  if name is not 'saturizd', there will be infinite recursion calls
-  }
+  } 
+  // return fix_spelling(name); //  if name is not 'saturizd', there will be infinite recursion calls
   return name;
   // return 'name'; // it return string 'name', not variable name... so it's return always same value - 'name'...  
 }
