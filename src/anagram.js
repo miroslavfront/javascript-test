acf5101901ae4ec26a2d34b87c6c8b133cd45a49
Vislavski <wallsorim@yahoo.ca>
2018-05-07 19:03:46 +0200

    solution

--- src/anagram.js
+++ src/anagram.js
@@ -6,7 +6,13 @@
      "foefet" is an anagram of "toffee"
      "Buckethead" is an anagram of "DeathCubeK"
 */
+function prepareString (str) {
+  // convert string to lowercase
+  // remove white spaces
+  // sort string (split/convert string to array, sort, and join back to string)
+  return str.toLowerCase().replace(/[^a-z\d]/g,'').split('').sort().join('');
+}
 
 module.exports = function isAnagram(test, original) {
-
+  return prepareString(test) === prepareString(original);
 };
--- src/name_me.js
+++ src/name_me.js
@@ -12,5 +12,9 @@
 module.exports = function NameMe(first, last) {
     this.firstName = first;
     this.lastName = last;
-    return {name: this.firstName + ' ' + this.lastName};
+    return {
+      name: this.firstName + ' ' + this.lastName,
+      firstName: this.firstName,
+      lastName: this.lastName
+    };
 }
